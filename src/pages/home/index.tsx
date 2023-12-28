bd906c48bc4840b4d0f3429c71adce5d759cc2de
Ali Dehghan <mr.niri@yahoo.com>
2023-12-28 21:04:44 +0330

    fix home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,21 @@
 import Button from "@mui/material/Button";
 import type { FC } from "react";
-
+import { useNavigate } from "react-router-dom";
+import strings from "./strings.json";
+import HomeWrapper from "./styled";
 const HomePage: FC = () => {
-  return <Button>Break the world جهان را خراب کن ۱۲۳۴۵</Button>;
+  const navigate = useNavigate();
+
+  const onButtonClickHandler = () => {
+    navigate("/dashboard");
+  };
+  return (
+    <HomeWrapper>
+      <Button onClick={onButtonClickHandler} variant="contained">
+        {strings.marketPage}
+      </Button>
+    </HomeWrapper>
+  );
 };
 
 export default HomePage;
---
+++ src/pages/home/styled.ts
@@ -0,0 +1,10 @@
+import { Box, styled } from "@mui/material";
+
+const HomeWrapper = styled(Box)(({ theme }) => ({
+  flex: 1,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+}));
+
+export default HomeWrapper;
